8ae987f50b4319e3544fc13a25c32c4c653cf138
HasibRahman <hasib.fagun@gmail.com>
2017-05-01 20:41:22 +0600

    Added Stock Codes and Prices in Web Service

---
+++ StockExchangeSimulatorApp/StockExchangeSimulatorApp/Stock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockExchangeSimulatorApp
+{
+    public class Stock
+    {
+        public string StockCode { get; set; }
+        public int StockPrice { get; set; }
+
+    }
+}
--- StockExchangeSimulatorApp/StockExchangeSimulatorApp/StockExchangeService.asmx.cs
+++ StockExchangeSimulatorApp/StockExchangeSimulatorApp/StockExchangeService.asmx.cs
@@ -18,9 +18,29 @@ namespace StockExchangeSimulatorApp
     {
 
         [WebMethod]
-        public string HelloWorld()
+        public List<Stock> GetStockPrices()
         {
-            return "Hello World";
+            Random r = new Random();
+            List<Stock> stockList = new List<Stock>
+            {
+                new Stock {StockCode = "AAPL",StockPrice =r.Next(1,1000) },
+                new Stock {StockCode = "IYW",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "AUY",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "QQQ",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "MSFT",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "XLK",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "ACN",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "TGT",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "INTC",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "VGT",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "GOOG",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "AHX",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "KO",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "BRKA",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "TXN",StockPrice = r.Next(1,1000)},
+                new Stock {StockCode = "AZS",StockPrice = r.Next(1,1000)},
+            };
+            return stockList;
         }
     }
 }
